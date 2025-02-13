077d03a5568594eb7bc5e6b7103ca1b3b8d29110
Ian <ianwohlers@gmail.com>
2025-02-13 01:10:48 -0800

    Add sketchy UI prompting as well

--- Assets/Scripts/Combatants/Behavior/PlayerBehavior.cs
+++ Assets/Scripts/Combatants/Behavior/PlayerBehavior.cs
@@ -13,26 +13,26 @@ public class PlayerBehavior : CombatantBehavior
 {
     public async override Task<Action> Decide()
     {
-        Action chosen = DecideAction();
+        Action chosen = await DecideAction();
+        Debug.Log($"Chose action {chosen.GetType().Name}");
         await Populate((dynamic)chosen);
         return chosen;
     }
 
-    private Action DecideAction() {
-        // TODO -> Add a UI picker to decide which action to use
-        return GetComponents<Action>().First();
+    private async Task<Action> DecideAction() {
+        Action[] choices = GetComponents<Action>();
+        int idx = await UIGenerator.instance.SelectTextChoice(choices.Select(a => a.GetType().Name).ToList());
+        return choices[idx];
     }
 
     /*
     We dispatch to most specific Populate method (via dynamic)
     */
     public override Task Populate(Action action) {
-        Debug.Log("No-op populating");
         return Task.CompletedTask; // No-Op
     }
 
     public async Task Populate(Teleport action) {
-        Debug.Log("Populating teleport");
         Board board = action.GetComponent<GridElement>().GetBoard();
         var selection = await UIGenerator.instance.SelectTile(board);
         action.Build(selection);
--- Assets/Scripts/UI/UIGenerator.cs
+++ Assets/Scripts/UI/UIGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -19,15 +20,37 @@ public class UIGenerator : MonoBehaviour
         cursor.gameObject.SetActive(false);
     }
 
+    // This interleaving of async and coroutine is a bit weird
+    public async Task<int> SelectTextChoice(List<string> choices) {
+        var tcs = new TaskCompletionSource<int>();
+        StartCoroutine(WaitForTextChoice(choices, tcs));
+        return await tcs.Task;
+    }
+
+    private IEnumerator WaitForTextChoice(List<string> choices, TaskCompletionSource<int> tcs)
+    {
+        KeyCode keyBase = KeyCode.Q;
+        KeyCode[] keycodes = choices.Select((_, idx) => keyBase + idx).ToArray();
+
+        string prompt = string.Join(", ", choices.Select((choice, idx) => $"{choice} - ({keycodes[idx]})"));
+        Debug.Log($"Choose: {prompt}");
+        while (!keycodes.Any(k => Input.GetKeyDown(k))) {
+            yield return null;
+        }
+        var chosen = keycodes.First(k => Input.GetKeyDown(k));
+        tcs.SetResult(keycodes.ToList().IndexOf(chosen));
+    }
+
+
     public async Task<Vector2Int> SelectTile(Board b) {
         cursor.board = b;
         cursor.selection = null;
         var tcs = new TaskCompletionSource<Vector2Int>();
-        StartCoroutine(WaitForSelection(tcs));
+        StartCoroutine(WaitForTileSelection(tcs));
         return await tcs.Task;
     }
 
-    private IEnumerator WaitForSelection(TaskCompletionSource<Vector2Int> tcs)
+    private IEnumerator WaitForTileSelection(TaskCompletionSource<Vector2Int> tcs)
     {
         cursor.gameObject.SetActive(true);
         yield return new WaitUntil(() => cursor.selection.HasValue);
