77712e1399e9a5a41acec86d6131fdffdc5dadc8
Ian <ianwohlers@gmail.com>
2025-02-23 18:44:34 -0800

    Add draft for combinations

--- Assets/Scripts/Statuses/Status.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Status : Actor
-{
-    // TODO - Merge alike logic between player, tile status. (Combine? Act?)
-}
