db9e1bc4dd392a3519161754cdfaf5040a4d0fec
Ian <ianwohlers@gmail.com>
2025-02-27 23:57:56 -0800

    Implement CanMove

--- Assets/Scripts/Statuses/Primitives/Stun.cs
+++ Assets/Scripts/Statuses/Primitives/Stun.cs
@@ -2,7 +2,7 @@ using System.Collections;
 
 public class Stun : CombatantMutation<bool>
 {
-    public override SubscribableMutation<bool> Target() => Parent.CanMove;
+    public override MutatableValue<bool> Target() => Parent.CanMove;
     public override bool Mutate(bool original) {
         return false;
     }
